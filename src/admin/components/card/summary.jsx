60ec7a3d405c2e3ec3a9520396d789e9b4727f50
aminhas309@hotmail.com <hamzaidrees083@gmail.com>
2023-12-09 16:02:56 +0500

    Summary Card

---
+++ src/admin/components/card/summary.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import SubCard from '../card/subcard';
+
+const SummaryCard = () => {
+  return (
+    <Card
+      sx={{
+        width: '100%',
+        maxWidth: 440,
+        height: 280,
+        position: 'relative',
+        borderRadius: 5,
+      }}
+    >
+      <CardContent>
+        <Typography
+          variant="h6"
+          gutterBottom
+          sx={{ textAlign: 'center', marginBottom: '5px', color: 'black' }}
+        >
+          Performance Summary, This Month
+        </Typography>
+      </CardContent>
+
+      <CardContent
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            width: '100%',
+          }}
+        >
+          <SubCard content="Tenant's Registered This Month" />
+          <SubCard content="User's Registered This Month" />
+        </div>
+      </CardContent>
+
+      <CardContent
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            width: '100%',
+          }}
+        >
+          <SubCard content="Driver's Registered This Month" />
+          <SubCard content="Vehicle's Registered This Month" />
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default SummaryCard;
