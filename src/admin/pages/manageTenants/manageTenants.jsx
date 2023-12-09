e1b511902558341b202534e994b173a3a3f49ac5
aminhas309@hotmail.com <hamzaidrees083@gmail.com>
2023-12-09 16:24:14 +0500

    Run Pretty For Formating

--- src/admin/pages/manageTenants/manageTenants.jsx
+++ src/admin/pages/manageTenants/manageTenants.jsx
@@ -7,50 +7,72 @@ import TableRow from '@mui/material/TableRow';
 import TableSortLabel from '@mui/material/TableSortLabel';
 import { Button } from '@mui/material';
 
-
 const ManageTenants = () => {
   return (
     <>
-      <div style={{ margin: '30px', width: '1055px', height: '430px', display: 'flex', flexDirection: 'column' }}>
-        <div sx={{ width: '1000px', height: '30', dispaly: 'flex', flexDirection: 'row', marginLeft: '5px', color: 'white' }}>
-          <Button variant="text" sx={{ color: '#7E62D7', borderBottom: '2px solid #7E62D7' }}>Recent Booking</Button>
-          <Button variant="text" sx={{ color: '#7E62D7' }}>Vehicle Details</Button>
-          <Button variant="text" sx={{ color: '#7E62D7' }}>Driver Details</Button>
+      <div
+        style={{
+          margin: '30px',
+          width: '1055px',
+          height: '430px',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <div
+          sx={{
+            width: '1000px',
+            height: '30',
+            dispaly: 'flex',
+            flexDirection: 'row',
+            marginLeft: '5px',
+            color: 'white',
+          }}
+        >
+          <Button
+            variant="text"
+            sx={{ color: '#7E62D7', borderBottom: '2px solid #7E62D7' }}
+          >
+            Recent Booking
+          </Button>
+          <Button variant="text" sx={{ color: '#7E62D7' }}>
+            Vehicle Details
+          </Button>
+          <Button variant="text" sx={{ color: '#7E62D7' }}>
+            Driver Details
+          </Button>
           <hr></hr>
         </div>
 
         <div>
-          <Table sx={{ dispaly: 'flex', flexDirection: 'row', }}>
-            <TableHead sx={{ backgroundColor: '#7E62D7'}}>
+          <Table sx={{ dispaly: 'flex', flexDirection: 'row' }}>
+            <TableHead sx={{ backgroundColor: '#7E62D7' }}>
               <TableRow>
                 <TableCell sx={{ color: 'white' }}>
                   User
                   <TableSortLabel
                     active={true}
                     direction="asc"
-                  >
-                  </TableSortLabel>
+                  ></TableSortLabel>
                 </TableCell>
-               
+
                 <TableCell sx={{ color: 'white' }}>
-                Date
+                  Date
                   <TableSortLabel
                     active={true}
                     direction="desc"
-                  >
-                  </TableSortLabel>
+                  ></TableSortLabel>
                 </TableCell>
                 <TableCell sx={{ color: 'white' }}>
                   Payment
                   <TableSortLabel
                     active={true}
                     direction="asc"
-                  >
-                  </TableSortLabel>
+                  ></TableSortLabel>
                 </TableCell>
               </TableRow>
             </TableHead>
-            <TableBody >
+            <TableBody>
               <TableRow>
                 <TableCell>Row 1</TableCell>
                 <TableCell>Row 1</TableCell>
@@ -64,9 +86,7 @@ const ManageTenants = () => {
             </TableBody>
           </Table>
         </div>
-
       </div>
-
     </>
   );
 };
