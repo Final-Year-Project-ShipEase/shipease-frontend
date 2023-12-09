ff734f2cc07c8469205e8526fb54be745096c420
M-Muntazer-Mehdi <muntazer.mehdi.rizvi@gmail.com>
2023-12-09 23:30:51 +0500

    Tenant Management FrontEnd

--- src/admin/components/navbar/navbar.jsx
+++ src/admin/components/navbar/navbar.jsx
@@ -2,50 +2,116 @@ import React from 'react';
 import { AppBar, Box, Toolbar } from '@mui/material';
 import { appBarStyles } from './config.js';
 import UserInfo from './userInfo';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
 
-const Navbar = () => {
-  return (
-    <Box
-      sx={{
-        backgroundColor: '#ffffff',
-      }}
-    >
-      <AppBar position="fixed" elevation={3} sx={appBarStyles}>
-        <Toolbar>
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              width: '100%',
-            }}
-          >
-            <Box
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
-              <img
-                src={require('../../resources/shipease.png')}
-                alt="ShipEase-logo"
-              />
-            </Box>
-            <Box
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
-              <UserInfo />
-            </Box>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-};
+              const Navbar = () => {
+                return (
+                  <Box
+                    sx={{
+                      backgroundColor: '#ffffff',
+                    }}
+                  >
+                    <AppBar position="fixed" elevation={3} sx={appBarStyles}>
+                      <Toolbar>
+                        <Box
+                          sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            width: '100%',
+                          }}
+                        >
+                          <Box
+                            sx={{
+                              display: 'flex',
+                              alignItems: 'center',
+                              justifyContent: 'center',
+                              paddingLeft: '10px',
+                              paddingRight: '45px',
+                              borderRight: '1px solid #ccc',
+                            }}
+                          >
+                            <img
+                              src={require('../../resources/shipease.png')}
+                              alt="ShipEase-logo"
+                            />
+                          </Box>
+                          <Box
+                            sx={{
+                              display: 'flex',
+                              alignItems: 'center',
+                              justifyContent: 'center',
+                            }}
+                          >
+                            <label>
+                              <span
+                                style={{
+                                  color: 'rgba(126, 98, 215, 1)',
+                                  fontSize: '1.2rem',
+                                  fontWeight: 'bold',
+                                  marginLeft: "-140px"
+                                }}
+                              >
+                                Tenant 
+                              </span>
+                              <span
+                                style={{
+                                  color: '#565656',
+                                  fontSize: '1.2rem',
+                                  fontWeight: 'bold',
+                                }}
+                              >
+                                 _Managemant
+                              </span>
+                            </label>
+                          </Box>
+                          <Box
+                            sx={{
+                              display: 'flex',
+                              alignItems: 'center',
+                              justifyContent: 'flex-end', // Aligns the content to the right
+                            }}
+                          >
+                            <InputBase
+                              sx={{
+                                border: '1px solid #ccc',
+                                borderRadius: '14px',
+                                backgroundColor: 'rgba(126, 98, 215, 0.5)',
+                                padding: '2px',
+                                paddingLeft: '15px',
+                                marginTop: '-2%',
+                                width: '300px',
+                                marginLeft: 'auto',
+                                color: "white"
+                              }}
+                              placeholder="Search..."
+                              inputProps={{ 'aria-label': 'search' }}
+                              startAdornment={
+                                <SearchIcon
+                                  sx={{
+                                    color: 'white',
+                                    marginRight: '18px',
+                                  }}
+                                />
+                              }
+                            />
+                          </Box>
+                          <Box
+                            sx={{
+                              display: 'flex',
+                              alignItems: 'center',
+                              justifyContent: 'center',
+                            }}
+                          >
+                            <UserInfo />
+                          </Box>
+                        </Box>
+                      </Toolbar>
+                    </AppBar>
+                  </Box>
+                );
+              };
 
-export default Navbar;
+              export default Navbar;
+            
--- src/admin/pages/manageTenants/manageTenants.jsx
+++ src/admin/pages/manageTenants/manageTenants.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Grid } from '@mui/material';
 import Navbar from '../../../admin/components/navbar/navbar';
 import Sidebar from '../../../admin/components/sidebar/sidebar';
+import { Outlet } from 'react-router-dom';
 
 const ManageTenants = () => {
   const leftSpan = 1.5;
+  const rigthSpan = 12 - leftSpan;
   return (
     <div>
       <header>
@@ -13,6 +15,19 @@ const ManageTenants = () => {
           <Grid item md={leftSpan}>
             <Sidebar leftSpan={leftSpan} />
           </Grid>
+        </Grid>
+
+
+        <Grid
+          item
+          md={rigthSpan}
+          sx={
+            {/*mt: '7vh',*/
+            backgroundColor: '#DFDFDF',
+            minHeight: '93vh',
+          }}
+        >
+          <Outlet context={[rigthSpan, leftSpan]} />
         </Grid>
       </header>
       <main>
