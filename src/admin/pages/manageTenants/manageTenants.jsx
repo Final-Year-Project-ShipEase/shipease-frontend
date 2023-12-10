c0523f8a39ca7c5bb795e3a56f273857f76b3571
aminhas309@hotmail.com <hamzaidrees083@gmail.com>
2023-12-10 06:08:25 +0500

    Tenant Management Module

--- src/admin/pages/manageTenants/manageTenants.jsx
+++ src/admin/pages/manageTenants/manageTenants.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Box } from '@mui/material';
+import TableData from '../driversApproval/components/table/table'
+import TenantDetails from './recentBookingComponent/tenantDetails';
+import TabComponent from './tabComponent';
 
 const ManageTenants = () => {
   return (
-    <>      
-    </>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100px',
+        padding: 2,
+      }}
+    >
+
+      <Box sx={{marginBottom: '-10px', width:'100%', height:'280px', borderRadius:'10px'}}>
+        <TenantDetails />
+      </Box>
+
+      <Box sx={{ mt: '32px', borderTop: '1px dashed #BEBEBE' }}>
+        <TabComponent />
+      </Box>
+    </Box>
   );
 };
 
