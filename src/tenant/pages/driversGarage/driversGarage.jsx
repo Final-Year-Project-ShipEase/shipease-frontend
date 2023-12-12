67200c73bbbb2bc5b750bbb076703ce02581e37a
Abdullah Minhas <aminhas309@hotmail.com>
2023-12-12 18:35:20 +0500

    FYP-34 Garage Managment Missing Files Fixes

--- src/tenant/pages/driversGarage/driversGarage.jsx
+++ src/tenant/pages/driversGarage/driversGarage.jsx
@@ -11,7 +11,6 @@ const DriversGarage = () => {
       sx={{
         display: 'flex',
         flexDirection: 'column',
-        height: '100px',
         padding: 2,
       }}
     >
@@ -19,7 +18,7 @@ const DriversGarage = () => {
         sx={{
           marginBottom: '-10px',
           width: '100%',
-          height: '380px',
+          height: '100%',
           borderRadius: '10px',
           boxShadow: '0px 2px 14px rgba(0, 0, 0, 1)', // Add drop shadow
         }}
