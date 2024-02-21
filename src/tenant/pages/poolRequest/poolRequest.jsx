bf76d2a2374b2389a9a2426b4bae7e625e0f17e5
M-Muntazer-Mehdi <muntazer.mehdi.rizvi@gmail.com>
2024-02-21 18:03:54 +0500

    pool request modal component

--- src/tenant/pages/poolRequest/poolRequest.jsx
+++ src/tenant/pages/poolRequest/poolRequest.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import PageHeader from './pageHeader';
+import PoolRequestInformation from './poolRequestCard/poolrequestInformation';
 
 const poolRequest = () => {
     return (
         <>
             <PageHeader></PageHeader>
+            <PoolRequestInformation></PoolRequestInformation>
         </>
     )
 };
