068ea707177fa21664a285b48b05bd53a7b4cfde
minhas309 <aminhas309@hotmail.com>
2024-03-24 03:17:13 +0500

    Cards modified

--- src/tenant/pages/booking/components/card/bid.jsx
+++ src/tenant/pages/booking/components/card/bid.jsx
@@ -2,77 +2,97 @@ import React from 'react';
 import { Box } from '@mui/material';
 import { useState, useEffect } from 'react';
 import { useTheme } from '@mui/material/styles';
-import speaker from './resources/icons8-loudspeaker-32.png';
+import CampaignIcon from '@mui/icons-material/Campaign';
 import { useBookingService } from '../../../../../services/bookingServices';
 
 const Bid = () => {
-    const theme = useTheme();
-    const [bid, setBid] = useState(0);
-    const { getBookingList } = useBookingService();
+  const theme = useTheme();
+  const [bid, setBid] = useState(0);
+  const { getBookingList } = useBookingService();
 
-    useEffect(() => {
-        const fetchBooking = async () => {
-            try {
-                const response = await getBookingList();
-                const bids = response.filter(booking => booking.status === 'bid');
-                setBid(bids.length);      
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        fetchBooking();
-    }, []);
-    return (
-        <Box sx={{
-            display:'flex',
-            flexDirection:'row',
-            justifyContent:'space-between',
-            backgroundColor: theme.palette.primary.backgroundColor,
-            width: '15%',
-            height: '10%',
-            borderRadius: '10px',
-            marginLeft: '60px',
-            marginBottom: '20px',
-        }}>
-            <Box sx={{
-                display: 'flex',
-                flexDirection: 'row',
-                paddingLeft:'30px'
-            }}>
-                <Box sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                }}>
-                    <img src={speaker} alt='Bidding' />
-                </Box>
-            </Box>
-
-            <Box sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                paddingRight:'70px'
-            }}>
-                <Box sx={{
-                    display: 'flex',
-                    fontSize: '20px',
-                    fontWeight: 'bold',
-                    color: theme.palette.primary.text,
-                }}>
-                    {bid}
-                </Box>
-                <Box sx={{
-                    display: 'flex',
-                    fontSize: '12px',
-                    color: theme.palette.primary.text,
-                }}>
-                    Bids
-                </Box>
-
-            </Box>
+  useEffect(() => {
+    const fetchBooking = async () => {
+      try {
+        const response = await getBookingList();
+        const bids = response.filter((booking) => booking.status === 'bid');
+        setBid(bids.length);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchBooking();
+  }, []);
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: theme.palette.primary.backgroundColor,
+        width: '11%',
+        height: '10%',
+        borderRadius: '10px',
+        marginLeft: '3%',
+        marginBottom: '3%',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '40%',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            color: theme.palette.primary.color1,
+          }}
+        >
+          <CampaignIcon
+            sx={{
+              color: theme.palette.primary.text,
+            }}
+          />
         </Box>
-    )
+      </Box>
+
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '60%',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            fontSize: '20px',
+            fontWeight: 'bold',
+            color: theme.palette.primary.text,
+          }}
+        >
+          {bid}
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            fontSize: '12px',
+            color: theme.palette.primary.text,
+          }}
+        >
+          Bids
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Bid;            
